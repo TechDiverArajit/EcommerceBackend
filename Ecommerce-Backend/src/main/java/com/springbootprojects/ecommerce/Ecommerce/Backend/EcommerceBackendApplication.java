3baf468ef75c86a60a85e56b60da20f3359a71b6
arajitdev <arajitdebnathofficial@gmail.com>
2026-03-02 23:46:12 +0530

    first commit

---
+++ Ecommerce-Backend/src/main/java/com/springbootprojects/ecommerce/Ecommerce/Backend/EcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springbootprojects.ecommerce.Ecommerce.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendApplication.class, args);
+	}
+
+}
