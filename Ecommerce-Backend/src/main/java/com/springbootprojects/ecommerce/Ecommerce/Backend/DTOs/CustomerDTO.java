60640b2a8222ba110db48b68face6ee131690c7b
arajitdev <arajitdebnathofficial@gmail.com>
2026-03-04 23:49:22 +0530

    order entity and customer dto added

---
+++ Ecommerce-Backend/src/main/java/com/springbootprojects/ecommerce/Ecommerce/Backend/DTOs/CustomerDTO.java
@@ -0,0 +1,4 @@
+package com.springbootprojects.ecommerce.Ecommerce.Backend.DTOs;
+
+public class CustomerDTO {
+}
---
+++ Ecommerce-Backend/src/main/java/com/springbootprojects/ecommerce/Ecommerce/Backend/Entities/OrderEntity.java
@@ -0,0 +1,4 @@
+package com.springbootprojects.ecommerce.Ecommerce.Backend.Entities;
+
+public class OrderEntity {
+}
